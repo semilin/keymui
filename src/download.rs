18ca9645d21e2e9c979fce3f63b1bef537b79461
semi <semilin@disroot.org>
2023-11-25 14:29:01 -0800

    replace repetitive download functions

--- src/download.rs
+++ src/download.rs
@@ -22,50 +22,22 @@ pub fn download_files(data_dir: &Path) -> Result<(), Box<dyn std::error::Error>>
 	.user_agent(APP_USER_AGENT)
 	.build()?;
 
-    download_layouts(&data_dir.join("layouts"), &client)?;
-    download_metrics(&data_dir.join("metrics"), &client)?;
-    download_corpora(&data_dir.join("corpora"), &client)?;
+    download_repo(&client, &data_dir.join("layouts"), "https://api.github.com/repos/semilin/km_layouts/contents/")?;
+    download_repo(&client, &data_dir.join("metrics"), "https://api.github.com/repos/semilin/km_metric_data/contents/")?;
+    download_repo(&client, &data_dir.join("corpora"), "https://api.github.com/repos/semilin/km_corpora/contents/")?;
     Ok(())
 }
 
-pub fn download_layouts(layouts_dir: &Path, client: &Client) -> Result<(), Box<dyn std::error::Error>> {
-    let resp = client.get("https://api.github.com/repos/semilin/km_layouts/contents/").send();
+pub fn download_repo(client: &Client, directory: &Path, url: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let resp = client.get(url).send();
     let data = resp?.json::<Vec<GithubFileData>>()?;
     
     for filedata in data {
 	println!("downloading {}", filedata.name);
-	if let Ok(layout_json) = client.get(filedata.download_url).send() {
-	    let result = fs::write(layouts_dir.join(filedata.name), layout_json.text()?);
+	if let Ok(contents) = client.get(filedata.download_url).send() {
+	    let result = fs::write(directory.join(filedata.name), contents.text()?);
 	    println!("{:?}", result);
 	};
-    }
-    Ok(())
-}
-
-pub fn download_metrics(metrics_dir: &Path, client: &Client) -> Result<(), Box<dyn std::error::Error>> {
-    let resp = client.get("https://api.github.com/repos/semilin/km_metric_data/contents/").send();
-    let data = resp?.json::<Vec<GithubFileData>>()?;
-
-    for filedata in data {
-	println!("downloading {}", filedata.name);
-	if let Ok(metrics_json) = client.get(filedata.download_url).send() {
-	    let result = fs::write(metrics_dir.join(filedata.name), metrics_json.text()?);
-	    println!("{:?}", result);
-	}
-    }
-    Ok(())
-}
-
-pub fn download_corpora(corpora_dir: &Path, client: &Client) -> Result<(), Box<dyn std::error::Error>> {
-    let resp = client.get("https://api.github.com/repos/semilin/km_corpora/contents/").send();
-    let data = resp?.json::<Vec<GithubFileData>>()?;
-
-    for filedata in data {
-	println!("downloading {}", filedata.name);
-	if let Ok(corpus_json) = client.get(filedata.download_url).send() {
-	    let result = fs::write(corpora_dir.join(filedata.name), corpus_json.text()?);
-	    println!("{:?}", result);
-	}
     }
     Ok(())
 }
