460bb27fd6c8d92c95a396f18adda44391529797
semi <semilin@disroot.org>
2024-02-16 19:26:35 -0800

    cargo fmt

--- src/download.rs
+++ src/download.rs
@@ -1,8 +1,8 @@
 use reqwest::blocking::Client;
 use serde::Deserialize;
-use std::time::Duration;
-use std::path::Path;
 use std::fs;
+use std::path::Path;
+use std::time::Duration;
 
 #[derive(Deserialize, Debug)]
 struct GithubFileData {
@@ -10,34 +10,46 @@ struct GithubFileData {
     download_url: String,
 }
 
-static APP_USER_AGENT: &str = concat!(
-    env!("CARGO_PKG_NAME"),
-    "/",
-    env!("CARGO_PKG_VERSION"),
-);
+static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
 
 pub fn download_files(data_dir: &Path) -> Result<(), Box<dyn std::error::Error>> {
     let client = Client::builder()
-	.timeout(Duration::from_secs(10))
-	.user_agent(APP_USER_AGENT)
-	.build()?;
+        .timeout(Duration::from_secs(10))
+        .user_agent(APP_USER_AGENT)
+        .build()?;
 
-    download_repo(&client, &data_dir.join("layouts"), "https://api.github.com/repos/semilin/km_layouts/contents/")?;
-    download_repo(&client, &data_dir.join("metrics"), "https://api.github.com/repos/semilin/km_metric_data/contents/")?;
-    download_repo(&client, &data_dir.join("corpora"), "https://api.github.com/repos/semilin/km_corpora/contents/")?;
+    download_repo(
+        &client,
+        &data_dir.join("layouts"),
+        "https://api.github.com/repos/semilin/km_layouts/contents/",
+    )?;
+    download_repo(
+        &client,
+        &data_dir.join("metrics"),
+        "https://api.github.com/repos/semilin/km_metric_data/contents/",
+    )?;
+    download_repo(
+        &client,
+        &data_dir.join("corpora"),
+        "https://api.github.com/repos/semilin/km_corpora/contents/",
+    )?;
     Ok(())
 }
 
-pub fn download_repo(client: &Client, directory: &Path, url: &str) -> Result<(), Box<dyn std::error::Error>> {
+pub fn download_repo(
+    client: &Client,
+    directory: &Path,
+    url: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
     let resp = client.get(url).send();
     let data = resp?.json::<Vec<GithubFileData>>()?;
-    
+
     for filedata in data {
-	println!("downloading {}", filedata.name);
-	if let Ok(contents) = client.get(filedata.download_url).send() {
-	    let result = fs::write(directory.join(filedata.name), contents.text()?);
-	    println!("{:?}", result);
-	};
+        println!("downloading {}", filedata.name);
+        if let Ok(contents) = client.get(filedata.download_url).send() {
+            let result = fs::write(directory.join(filedata.name), contents.text()?);
+            println!("{:?}", result);
+        };
     }
     Ok(())
 }
