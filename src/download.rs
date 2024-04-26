ab4c69966090c24460d5e5a799f19e23f681539c
semi <semilin@disroot.org>
2024-04-26 15:01:51 -0700

    use minreq instead of reqwest

--- src/download.rs
+++ src/download.rs
@@ -1,8 +1,6 @@
-use reqwest::blocking::Client;
 use serde::Deserialize;
 use std::fs;
 use std::path::Path;
-use std::time::Duration;
 
 #[derive(Deserialize, Debug)]
 struct GithubFileData {
@@ -13,41 +11,35 @@ struct GithubFileData {
 static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
 
 pub fn download_files(data_dir: &Path) -> Result<(), Box<dyn std::error::Error>> {
-    let client = Client::builder()
-        .timeout(Duration::from_secs(10))
-        .user_agent(APP_USER_AGENT)
-        .build()?;
-
     download_repo(
-        &client,
         &data_dir.join("layouts"),
         "https://api.github.com/repos/semilin/km_layouts/contents/",
     )?;
     download_repo(
-        &client,
         &data_dir.join("metrics"),
         "https://api.github.com/repos/semilin/km_metric_data/contents/",
     )?;
     download_repo(
-        &client,
         &data_dir.join("corpora"),
         "https://api.github.com/repos/semilin/km_corpora/contents/",
     )?;
     Ok(())
 }
 
-pub fn download_repo(
-    client: &Client,
-    directory: &Path,
-    url: &str,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let resp = client.get(url).send();
+fn get(url: &str) -> minreq::Request {
+    minreq::get(url)
+        .with_header("User-Agent", APP_USER_AGENT)
+        .with_timeout(8)
+}
+
+pub fn download_repo(directory: &Path, url: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let resp = get(url).send();
     let data = resp?.json::<Vec<GithubFileData>>()?;
 
     for filedata in data {
         println!("downloading {}", filedata.name);
-        if let Ok(contents) = client.get(filedata.download_url).send() {
-            let result = fs::write(directory.join(filedata.name), contents.bytes()?);
+        if let Ok(contents) = get(&filedata.download_url).send() {
+            let result = fs::write(directory.join(filedata.name), contents.as_bytes());
             println!("{:?}", result);
         };
     }
